aeaceaa44c07ecfa8be09dd214f4206d144aefd9
madnoberson <baseddepartmentzx77@gmail.com>
2025-05-16 09:51:43 +0400

    Add `ExceptionMiddleware` for message consumer

--- src/connection_hub/presentation/message_consumer/broker.py
+++ src/connection_hub/presentation/message_consumer/broker.py
@@ -3,15 +3,29 @@
 # Licensed under the Personal Use License (see LICENSE).
 
 from faststream.nats import NatsBroker
+from faststream import ExceptionMiddleware
 
+from connection_hub.domain import DomainError
+from connection_hub.application import ApplicationError
 from .routes import router
 from .middlewares import OperationIdMiddleware, LoggingMiddleware
 
 
 def create_broker(nats_url: str) -> NatsBroker:
+    error_handlers = {
+        DomainError: lambda _: ...,
+        ApplicationError: lambda _: ...,
+    }
+    exception_middleware = ExceptionMiddleware(error_handlers)
+
+    middlewares = [
+        OperationIdMiddleware,
+        LoggingMiddleware,
+        exception_middleware,
+    ]
     broker = NatsBroker(
         nats_url,
-        middlewares=[OperationIdMiddleware, LoggingMiddleware],
+        middlewares=middlewares,
     )
 
     broker.include_router(router)
